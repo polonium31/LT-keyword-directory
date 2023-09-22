acbbf209539b7118f33fb887dfba204fe9f78757
polonium31 <jenu3181@gmail.com>
2023-09-22 10:53:03 +0530

    Project updated with spreedsheet

---
+++ README.md
@@ -0,0 +1,75 @@
+# Keyword-Targeted Article Directory
+
+## Project Description
+
+The Keyword-Targeted Article Directory is a web-based application designed to organize and manage articles based on specific keywords. It provides a user-friendly interface with different features for both users and administrators. This project aims to help users easily search for and access articles associated with specific keywords, while administrators have additional capabilities to manage the content.
+
+## Hosted Project link
+
+Open [LTkeyword Directory](https://ltkeyword.netlify.app/) to view it in your browser.
+
+### Features
+
+#### User Features
+
+1. **Search:** Users can search for articles by entering keywords. The system will display a list of articles related to the entered keyword.
+
+2. **Copy:** Users can copy the content of an article to their clipboard for easy reference.
+
+#### Admin Features
+
+1. **Search:** Administrators can search for articles by keywords, just like users.
+
+2. **Edit:** Administrators have the ability to edit articles. This can be useful for correcting errors, updating information, or making improvements to the content.
+
+3. **Delete:** Administrators can delete articles that are no longer relevant or need to be removed for any reason.
+
+4. **Copy:** Administrators, like users, can copy the content of an article to their clipboard.
+
+### How It Works
+
+1. **User Access:** Users can access the system and perform searches using keywords.
+
+2. **Admin Access:** Administrators have a separate login and can access the system with enhanced privileges.
+
+3. **Keyword-Targeted Articles:** Each keyword is associated with a specific article. When a user searches for a keyword, the system retrieves the corresponding article.
+
+4. **Editing and Deletion:** Administrators can make changes to articles or delete them as needed.
+
+5. **Copy Functionality:** Both users and administrators can easily copy the content of an article for their use.
+
+### Getting Started
+
+To use this application, follow these steps:
+
+1. **User Access:**
+
+   - Access the application through the provided URL.
+   - Enter a keyword in the search bar.
+   - View and copy the article content.
+
+2. **Admin Access:**
+   - Log in with administrator credentials.
+   - Search for articles using keywords.
+   - Edit or delete articles as necessary.
+   - Copy article content.
+
+### Technologies Used
+
+- React v18
+- Firebase 9
+
+## Contributors
+
+Thanks to these wonderful people who have contributed to this project:
+
+| [<img src="https://avatars.githubusercontent.com/u/68313150?v=4" width="100px;"/><br /><sub><b>Tanay Kumar deo</b></sub>](https://github.com/tanay388) | [<img src="https://avatars.githubusercontent.com/u/65034584?v=4" width="100px;"/><br /><sub><b>Jainish Patel</b></sub>](https://github.com/polonium31) |
+| :----------------------------------------------------------------------------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------: |
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+
+![All Contributors](https://img.shields.io/badge/all_contributors-2-orange.svg?style=flat-square)
+
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
---
+++ src/App.js
@@ -0,0 +1,279 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Import toast styles
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy, faLink } from "@fortawesome/free-solid-svg-icons";
+import logo from "./images/logo.png";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [articlesFound, setArticlesFound] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+  const [showList, setShowList] = useState(false);
+
+  useEffect(() => {
+    // Fetch data from the API
+    axios
+      .get("https://sheet.best/api/sheets/02d58853-59b1-4a0c-a756-ced9d1481faa")
+      .then((response) => {
+        const rawData = response.data;
+
+        // Filter out rows with empty "Keywords" or "URLs to Interlink" fields
+        const filteredData = rawData.filter(
+          (row) => row["Keywords"] && row["URLs to Interlink"]
+        );
+        setData(filteredData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filteredArticles = data.filter((item) =>
+      item["Keywords"]?.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setArticlesFound(filteredArticles.length > 0);
+  }, [data, searchQuery]);
+
+  const sortedBlogs = [...data].sort((a, b) =>
+    a["Keywords"]?.toLowerCase() > b["Keywords"]?.toLowerCase() ? 1 : -1
+  );
+
+  const toggleList = () => {
+    setShowList(!showList);
+    setSearchQuery("");
+    var getValue = document.getElementById("basic-url");
+    if (getValue.value !== "") {
+      getValue.value = "";
+    }
+  };
+
+  // Function to handle the next page
+  const nextPage = () => {
+    if (currentPage < Math.ceil(sortedBlogs.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Function to handle the previous page
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  // Function to copy the link to the clipboard and show a toast message
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text); // Copy the text to the clipboard
+    toast.success("Link copied to clipboard", {
+      position: "bottom-right",
+      autoClose: 2000,
+    });
+  };
+
+  const indexOfLastBlog = currentPage * itemsPerPage;
+  const indexOfFirstBlog = indexOfLastBlog - itemsPerPage;
+  const currentBlogs = sortedBlogs.slice(indexOfFirstBlog, indexOfLastBlog);
+
+  return (
+    <>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand">
+            <img src={logo} width="200" alt="Logo" />
+          </a>
+          <div className="navbar-text">
+            <h3 style={{ marginRight: "10px", color: "#000000" }}>
+              Keyword Directory
+            </h3>
+          </div>
+        </div>
+      </nav>
+      <div className="container p-4">
+        <div className="row justify-content-center">
+          <div className="col-10">
+            <div className="input-group">
+              <span className="input-group-text" id="basic-addon3">
+                Search
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="basic-url"
+                name="search"
+                onChange={(e) => {
+                  setSearchQuery(e.target.value);
+                  setShowList(false);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container p-2">
+        {searchQuery.trim() === "" ? (
+          <div className="text-center mt-4">
+            <h2>Welcome to the Articles Directory!</h2>
+            <p>Enter your search query above to find articles.</p>
+          </div>
+        ) : articlesFound ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Keyword</th>
+                <th>Article Link</th>
+                <th>Copy Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentBlogs.map((item, index) => (
+                <tr className="table-row" key={index}>
+                  <td style={{ width: "60%" }}>
+                    {item["Keywords"]
+                      ?.toLowerCase()
+                      .split(" ")
+                      .map(
+                        (word) => word.charAt(0).toUpperCase() + word.slice(1)
+                      )
+                      .join(" ")}
+                  </td>
+                  <td className="other">
+                    <a
+                      href={item["URLs to Interlink"]}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="link"
+                    >
+                      <FontAwesomeIcon icon={faLink} /> Link
+                    </a>
+                  </td>
+                  <td className="other">
+                    <button
+                      type="button"
+                      className="btn"
+                      onClick={() => copyToClipboard(item["URLs to Interlink"])}
+                    >
+                      <FontAwesomeIcon icon={faCopy} id="icon" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div className="text-center mt-4">
+            <h4>No articles found for the search query.</h4>
+          </div>
+        )}
+      </div>
+      {/* Key Glossary button */}
+      <div className="text-center mt-4">
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={toggleList}
+          style={{ fontWeight: "bold", marginBottom: "2%" }}
+        >
+          {showList ? "Hide List" : "Key Glossary"}
+        </button>
+      </div>
+      {/* Conditionally render the list of blogs */}
+      {showList && (
+        <div className="container p-2">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Keyword</th>
+                <th>Article Link</th>
+                <th>Copy Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentBlogs.map((item, index) => (
+                <tr className="table-row" key={index}>
+                  <td style={{ width: "60%" }}>
+                    {item["Keywords"]
+                      ?.toLowerCase()
+                      .split(" ")
+                      .map(
+                        (word) => word.charAt(0).toUpperCase() + word.slice(1)
+                      )
+                      .join(" ")}
+                  </td>
+                  <td className="other">
+                    <a
+                      href={item["URLs to Interlink"]}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="link"
+                    >
+                      <FontAwesomeIcon icon={faLink} /> Link
+                    </a>
+                  </td>
+                  <td className="other">
+                    <button
+                      type="button"
+                      className="btn"
+                      onClick={() => copyToClipboard(item["URLs to Interlink"])}
+                    >
+                      <FontAwesomeIcon icon={faCopy} id="icon" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {/* Pagination */}
+          <nav>
+            <ul className="pagination justify-content-center">
+              <li
+                className={`page-item ${currentPage === 1 ? "disabled" : ""}`}
+              >
+                <button className="page-link" onClick={prevPage}>
+                  &laquo; Prev
+                </button>
+              </li>
+              {Array(Math.ceil(sortedBlogs.length / itemsPerPage))
+                .fill()
+                .map((_, index) => (
+                  <li
+                    key={index}
+                    className={`page-item ${
+                      currentPage === index + 1 ? "active" : ""
+                    }`}
+                  >
+                    <button
+                      className="page-link"
+                      onClick={() => setCurrentPage(index + 1)}
+                    >
+                      {index + 1}
+                    </button>
+                  </li>
+                ))}
+              <li
+                className={`page-item ${
+                  currentPage === Math.ceil(sortedBlogs.length / itemsPerPage)
+                    ? "disabled"
+                    : ""
+                }`}
+              >
+                <button className="page-link" onClick={nextPage}>
+                  Next &raquo;
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
